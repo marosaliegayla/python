7f309ce384b0e6957207a5426da92aebce85cda6
Ma. Rosalie Gayla <112755129+marosaliegayla@users.noreply.github.com>
2023-11-23 11:30:09 +0800

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -1,21 +1,33 @@
-def catalog():
-    item1_price = 200.0
-    item2_price = 150.0
-    item3_price = 300.0
-    combo1_price = item1_price + item2_price - 0.1*(item1_price + item2_price)
-    combo2_price = item2_price + item3_price - 0.1*(item2_price + item3_price)
-    combo3_price = item1_price + item3_price - 0.1*(item1_price + item3_price)
-    gift_pack_price = item1_price + item2_price + item3_price - 0.25*(item1_price + item2_price + item3_price)
-
-    print("Online Store")
-    print("--------------------------------------")
-    print("Product(s)                                                   Price")    
-    print(f"Item 1                                                       {item1_price}")
-    print(f"Item 2                                                       {item2_price}")
-    print(f"Item 3                                                       {item3_price}")
-    print(f"Combo 1 (Item 1 + 2)                                         {combo1_price}")
-    print(f"Combo 2 (Item 2 + 3)                                         {combo2_price}")
-    print(f"Combo 3 (Item 1 + 3)                                         {combo3_price}")
-    print(f"Combo 4 (Item 1 + 2 + 3)                                     {gift_pack_price}")
-    print("——————————————————————————————————————")
-    print("For delivery contact: 98764678899")
+# Prices for individual items
+fried_chicken_price = 82.00
+fries_price = 50.00
+burger_price = 40.00
+coke_price = 53.00
+# Prices for combo meals with a 10% discount
+combo_one_price = (fried_chicken_price + fries_price) * 0.90
+combo_two_price = (fries_price + burger_price) * 0.90
+combo_three_price = (fried_chicken_price + burger_price) * 0.90
+# Price for the gift pack with a 25% discount
+gift_pack_price = (fried_chicken_price + fries_price + burger_price + coke_price) * 0.75
+# Print catalog header
+print("                        Kyu's Restaurant")
+print("--------------------------------------------------------------")
+print("Product(s)                                           Price")
+# Print individual item prices
+print("Fried Chicken                                      " + str(fried_chicken_price))
+print("Fries                                                    " + str(fries_price))
+print("Burger                                                 " + str(burger_price))
+print("Coke                                                   " + str(coke_price))
+# Print combo meal prices and descriptions
+print("Combo Meal One                                " + str(combo_one_price))
+print("     (Fried Chicken and Fries)")
+print("Combo Meal Two                                " + str(combo_two_price))
+print("     (Fries and Burger)")
+print("Combo Meal Three                              " + str(combo_three_price))
+print("     (Fried Chicken and Burger)")
+# Print gift pack price and description
+print("Gift Pack                                              " + str(gift_pack_price))
+print("     (Fried Chicken, Fries, Burger and Coke)")
+# Print catalog footer
+print("———————————————————————")
+print("visit kyusrestaurantdelivery.com for deliveries!")
